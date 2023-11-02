55a8b6ef3bf3fbc0c72b410cccb341d4a636387d
MrrSokea <133734800+MrrSokea@users.noreply.github.com>
2023-11-02 13:13:40 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,23 +2,23 @@ $(document).ready(function () {
     let myArray = JSON.parse(localStorage.getItem('myArray')) || [];
     var username = $("#username");
     var pass = $("#password");
-    var Type=$("#typeuser")
+    var Type = $("#typeuser")
     $("#submit").on("click", function () {
         if (pass.val() == "" && username.val() == "") {
             alert("please input username and password")
         } else if (pass.val() != "" && username.val() == "") {
             alert("please input username and password")
-        }else if (pass.val() == "" && username.val() != "") {
+        } else if (pass.val() == "" && username.val() != "") {
             alert("please input username and password")
         } else {
             if (myArray.length < 1) {
-                Pushdata({ name: username.val(), pass: pass.val(),Type: Type.val() })
+                Pushdata({ name: username.val(), pass: pass.val(), Type: Type.val() })
                 window.location.href = "allGame.html";
             } else {
-                if((username.val()==myArray[0].name && pass.val()==myArray[0].pass) && Type.val()==myArray[0].Type){
+                if ((username.val() == myArray[0].name && pass.val() == myArray[0].pass) && Type.val() == myArray[0].Type) {
                     window.location.href = "allGame.html";
-                }else{
-                    alert("Invalid username and password!!! you gmail is: <<"+myArray[0].name+">> and password is: <<"+myArray[0].pass+">> and Type User is <<"+myArray[0].Type+">>")
+                } else {
+                    alert("Invalid username and password!!! you gmail is: <<" + myArray[0].name + ">> and password is: <<" + myArray[0].pass + ">> and Type User is <<" + myArray[0].Type + ">>")
                 }
             }
 
@@ -29,24 +29,26 @@ $(document).ready(function () {
     function Pushdata(oject) {
         myArray.push(oject);
         localStorage.setItem('myArray', JSON.stringify(myArray));
-    }
+    } 
 
     console.log(myArray)
-
-    if(myArray[0].Type=="User"){
+    $(".brain").on("click", function () {
+        window.location.href = "index.html";
+        localStorage.clear();
+    })
+    $(".buy").addClass("d-none")
+    $(".buy").on("click", function () {
+        $(".inner-cart").empty()
+    })
+    if (myArray[0].Type == "User") {
         $(".key-print").addClass("d-none")
         $(".buy").removeClass("d-none")
         $(".type-user").text(myArray[0].Type)
-    }else{
+    } else {
         $(".buy").addClass("d-none")
         $(".key-print").removeClass("d-none")
         $(".type-user").text("Agent")
     }
-    $(".brain").on("click",function(){
-        localStorage.clear();             
-        window.location.href = "index.html";
-    })
-  $(".buy").on("click",function(){
-    $(".inner-cart").empty()
-  })
+
+
 });
--- js/print.js
+++ js/print.js
@@ -1,18 +1,33 @@
-
-
-$(".key-print").click(function(){
-    if(($("#TotalAmount").text()=="")||(parseInt($("#TotalAmount").text())<1000)){
-        $(this).removeAttr("data-target");
-    }else{
-        $(this).attr("data-target","#staticBackdrop");
-        $("#addRow").html(getRow());
-        function getRow(){
-            var tr=""
-            for(var i=0;i<$(".list-group-item").length;i++){
-                tr+=`<tr><td>`+$(".txt-f-arr1").eq(i).text()+`</td><td>`+$(".re_calu").eq(i).text()+`</td></tr>`
+$(".ticket").addClass("d-none");
+$(".buyBCart").on("click",function(){
+    $("#addRow").html(getRow());
+        function getRow() {
+            var tr = ""
+            for (var i = 0; i < $(".list-group-item").length; i++) {
+                tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
             }
             return tr;
         }
-        $("#pri-result").text($("#TotalAmount").text()+"R");
+        $("#pri-result").text($("#TotalAmount").text() + "R");
+})
+$("#randFive637").on("click",function(){
+    $("#addRow").html(getRow());
+        function getRow() {
+            var tr = ""
+            for (var i = 0; i < $(".list-group-item").length; i++) {
+                tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
+            }
+            return tr;
+        }
+        $("#pri-result").text($("#TotalAmount").text() + "R");
+})
+$(".key-print").click(function () {
+    if (($("#TotalAmount").text() == "") || (parseInt($("#TotalAmount").text()) < 1000)) {
+        $(this).removeAttr("data-target");
+    } else {
+        $(".ticket").removeClass("d-none");
+        window.print()
+        $(".ticket").addClass("d-none");
     }
+   
 })
