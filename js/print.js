9a670b3278f70186622dc20f402eb4fb1553c7a1
MrrSokea <133734800+MrrSokea@users.noreply.github.com>
2023-11-02 13:36:06 +0700

    Add files via upload

--- js/print.js
+++ js/print.js
@@ -1,25 +1,25 @@
-$(".ticket").addClass("d-none");
-$(".buyBCart").on("click",function(){
+
+$(".buyBCart").on("click", function () {
     $("#addRow").html(getRow());
-        function getRow() {
-            var tr = ""
-            for (var i = 0; i < $(".list-group-item").length; i++) {
-                tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
-            }
-            return tr;
+    function getRow() {
+        var tr = ""
+        for (var i = 0; i < $(".list-group-item").length; i++) {
+            tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
         }
-        $("#pri-result").text($("#TotalAmount").text() + "R");
+        return tr;
+    }
+    $("#pri-result").text($("#TotalAmount").text() + "R");
 })
-$("#randFive637").on("click",function(){
+$("#randFive637").on("click", function () {
     $("#addRow").html(getRow());
-        function getRow() {
-            var tr = ""
-            for (var i = 0; i < $(".list-group-item").length; i++) {
-                tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
-            }
-            return tr;
+    function getRow() {
+        var tr = ""
+        for (var i = 0; i < $(".list-group-item").length; i++) {
+            tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
         }
-        $("#pri-result").text($("#TotalAmount").text() + "R");
+        return tr;
+    }
+    $("#pri-result").text($("#TotalAmount").text() + "R");
 })
 $(".key-print").click(function () {
     if (($("#TotalAmount").text() == "") || (parseInt($("#TotalAmount").text()) < 1000)) {
@@ -27,5 +27,5 @@ $(".key-print").click(function () {
     } else {
         window.print()
     }
-   
+
 })
