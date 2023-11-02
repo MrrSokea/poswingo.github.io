924bab0d5d23266ec1539abf377818183adb4b16
MrrSokea <133734800+MrrSokea@users.noreply.github.com>
2023-11-02 15:35:43 +0700

    Add files via upload

--- js/print.js
+++ js/print.js
@@ -1,26 +1,55 @@
-
+function gotPrint(){
+    $("#addRow").html(getRow());
+    function getRow() {
+        var tr = ""
+        for (var i = 0; i < $(".list-group-item").length; i++) {
+            tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
+        }
+        return tr;
+    }
+    $("#pri-result").text($("#TotalAmount").text() + "R");
+}
 $(".buyBCart").on("click", function () {
-    $("#addRow").html(getRow());
-    function getRow() {
-        var tr = ""
-        for (var i = 0; i < $(".list-group-item").length; i++) {
-            tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
-        }
-        return tr;
-    }
-    $("#pri-result").text($("#TotalAmount").text() + "R");
+   gotPrint()
 })
+
 $("#randFive637").on("click", function () {
-    $("#addRow").html(getRow());
-    function getRow() {
-        var tr = ""
-        for (var i = 0; i < $(".list-group-item").length; i++) {
-            tr += `<tr><td>` + $(".txt-f-arr1").eq(i).text() + `</td><td>` + $(".re_calu").eq(i).text() + `</td></tr>`
-        }
-        return tr;
-    }
-    $("#pri-result").text($("#TotalAmount").text() + "R");
+   gotPrint()
 })
+
+$(".buyTwoD").on("click", function () {
+   gotPrint()
+})
+
+$(".buySigle").on("click", function () {
+   gotPrint()
+})
+$(".buySingle").on("click", function () {
+   gotPrint()
+})
+$(".buy-key-rang").on("click", function () {
+   gotPrint()
+})
+
+$(".buyArrang").on("click", function () {
+   gotPrint()
+})
+
+$(".buyThreeD").on("click", function () {
+   gotPrint()
+})
+
+$("#srandOne").on("click", function () {
+   gotPrint()
+})
+
+$("#srandFive").on("click", function () {
+   gotPrint()
+})
+$(".buyArrang3d").on("click", function () {
+   gotPrint()
+})
+
 $(".key-print").click(function () {
     if (($("#TotalAmount").text() == "") || (parseInt($("#TotalAmount").text()) < 1000)) {
         $(this).removeAttr("data-target");
